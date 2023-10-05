b832d1900d66b45d1c05fae400bfa9743b81b6e1
prakash6855 <prakashkumar68553495@gmail.com>
2023-10-05 23:06:40 +0530

    few solutions added

--- problem7/sol.js
+++ problem7/sol.js
@@ -1,8 +1,10 @@
-// var fruits = ['Apple', 'Banana', 'Orange'];
-// var findindex = fruits.indexOf('Banana');
-// console.log(findindex);
-
-
 var fruits = ['Apple', 'Banana', 'Orange'];
-var remfruits = fruits.splice(2, 1, 'watermelon');
+var findindex = fruits.indexOf('Banana');
+console.log(findindex);
+var newfruitsadded = fruits.splice(1, 1, 'Mango');
 console.log(fruits);
+
+
+// var fruits = ['Apple', 'Banana', 'Orange'];
+// var remfruits = fruits.splice(2, 1, 'watermelon');
+// console.log(fruits);
