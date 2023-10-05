f8b8762a8c55d9774b83b0a4dafec239c81c566f
prakash6855 <prakashkumar68553495@gmail.com>
2023-10-05 23:06:23 +0530

    few problems done

---
+++ problem7/sol.js
@@ -0,0 +1,8 @@
+// var fruits = ['Apple', 'Banana', 'Orange'];
+// var findindex = fruits.indexOf('Banana');
+// console.log(findindex);
+
+
+var fruits = ['Apple', 'Banana', 'Orange'];
+var remfruits = fruits.splice(2, 1, 'watermelon');
+console.log(fruits);
