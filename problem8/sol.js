69d275abe4f07956f85916b2348682a991346d75
prakash6855 <prakashkumar68553495@gmail.com>
2023-10-05 23:53:49 +0530

    few questions solved

---
+++ problem8/sol.js
@@ -0,0 +1,85 @@
+// You and your friends Tom, Jane, Peter and John got their final exam results. Your total
+// score is 85, Tom’s total score is 66, Jane’s total score is 95, Peter’s total score is 56
+// and John’s total score is 40. The grading chart is
+// 80 or above A grade
+// 60 or above B grade
+// 50 or above C grade
+// 40 or above D grade
+// 39 or less => F grade
+
+// Write a program to find your and your friends’ grades using if-else.
+
+const mymarks = 85;
+const tommarks = 66;
+const janemarks = 95;
+const petermarks = 56;
+const johnmarks = 40;
+
+//For me
+if(mymarks>=80){
+    console.log("A grade");
+}else if(mymarks>=60 && mymarks<80){
+    console.log("B Grade");
+}else if(mymarks>=50 && mymarks<60){
+    console.log("C Grade");
+}else if(mymarks>=40 && mymarks<50){
+    console.log("D Grade");
+}else{
+    console.log("F Grade");
+}
+
+//For Tom
+if(tommarks>=80){
+    console.log("A grade");
+}else if(tommarks>=60 && tommarks<80){
+    console.log("B Grade");
+}else if(tommarks>=50 && tommarks<60){
+    console.log("C Grade");
+}else if(tommarks>=40 && tommarks<50){
+    console.log("D Grade");
+}else{
+    console.log("F Grade");
+}
+//For Jane
+if(janemarks>=80){
+    console.log("A grade");
+}else if(janemarks>=60 && janemarks<80){
+    console.log("B Grade");
+}else if(janemarks>=50 && janemarks<60){
+    console.log("C Grade");
+}else if(janemarks>=40 && janemarks<50){
+    console.log("D Grade");
+}else{
+    console.log("F Grade");
+}
+
+
+
+//For Peter
+
+if(petermarks>=80){
+    console.log("A grade");
+}else if(petermarks>=60 && petermarks<80){
+    console.log("B Grade");
+}else if(petermarks>=50 && petermarks<60){
+    console.log("C Grade");
+}else if(petermarks>=40 && petermarks<50){
+    console.log("D Grade");
+}else{
+    console.log("F Grade");
+}
+
+
+
+//For John
+if(johnmarks>=80){
+    console.log("A grade");
+}else if(johnmarks>=60 && johnmarks<80){
+    console.log("B Grade");
+}else if(johnmarks>=50 && johnmarks<60){
+    console.log("C Grade");
+}else if(johnmarks>=40 && johnmarks<50){
+    console.log("D Grade");
+}else{
+    console.log("F Grade");
+}
---
+++ problem9/sol.js
@@ -0,0 +1,10 @@
+let a = 13;
+let b = 79;
+let c = 45;
+if(a>b && a>c){
+    console.log("A is greatest no");
+}else if(b>c && b>a){
+    console.log("B is greatest no");
+}else{
+    console.log("C is greatest among all");
+}
