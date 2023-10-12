ec3473c30e8dcac04f5a5bf41e0e7ef360596672
prakash6855 <prakashkumar68553495@gmail.com>
2023-10-12 23:32:53 +0530

    new practice set done

---
+++ problem18/sol.js
@@ -0,0 +1,10 @@
+let bookprice = [1100,200,300,80,44,500,250,211,300,80,111,222];
+let i =0;
+while(i<bookprice.length){
+    let price = bookprice[i];
+    if(price<200){
+        console.log(price);
+    }
+i++;
+}
+
