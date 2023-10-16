8a5f94b21262c876666d414f7132a46c6704346f
prakash6855 <prakashkumar68553495@gmail.com>
2023-10-16 22:59:24 +0530

    few codes added

---
+++ problem22/sol.js
@@ -0,0 +1,5 @@
+function squareNum(num){
+    let num1 = num*num;
+    return num1;
+}
+console.log(squareNum(5));
---
+++ problem23/sol.js
@@ -0,0 +1,11 @@
+
+const pizza = {
+
+    toppings: ['cheese', 'sauce', 'pepperoni'],
+  
+    crust: 'deep dish',
+  
+    Serves: 2
+  
+  }
+  console.log(pizza.toppings[2]);
