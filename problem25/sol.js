7fa81442dca33d4ef6d0d9b2a9809d4012b94b76
prakash6855 <prakashkumar68553495@gmail.com>
2023-10-18 22:19:12 +0530

    few changes done

---
+++ problem25/sol.js
@@ -0,0 +1,18 @@
+
+// Write a function called make_avg() which will take an three integers and return the average of those values.
+/*
+function make_avg(){
+    let num1 = 30;
+    let num2 = 40;
+    let num3 = 50;
+    let num = (num1+num2+num3)/3;
+    console.log(num);
+}
+make_avg();
+*/
+
+function make_avg(num1,num2,num3){
+let num = (num1+num2+num3)/3;
+return num;
+}
+console.log(make_avg(20,30,40));
